73321281a601ce1a3731967813a21161b165dc76
Yukifumi Itohiya <s23C1017KB@s.chibakoudai.jp>
2024-10-31 12:21:22 +0900

    Revert test.bash to original state

--- test.bash
+++ test.bash
@@ -5,9 +5,10 @@ ng () {
 	res=1
 }
 
-rea=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
+res=0
 
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "&LINENO"
+
+[ "${res}" = 0 ] && echo OK
 exit $res
