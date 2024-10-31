8bea07dee4699b4bb530c01c99343a32835fa631
Yukifumi Itohiya <s23C1017KB@s.chibakoudai.jp>
2024-10-31 12:45:53 +0900

    Revert test.bash to original state

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 
 ng () {
         echo ${1}行目が違うよ
@@ -8,7 +8,15 @@ ng () {
 res=0
 
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "&LINENO"
+[ "${out}" = 15 ] || ng "$LINENO"
 
-[ "${res}" = 0 ] && echo OK
+out=$(echo あ| ./plus)
+[ "$?" = 1]       || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
 exit $res
