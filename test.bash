5036bf9722dc3fcda1819d7e7ece686ae23292ab
Yukifumi Itohiya <s23C1017KB@s.chibakoudai.jp>
2024-11-07 13:31:46 +0900

    Add or update test.bash

--- test.bash
+++ test.bash
@@ -6,18 +6,36 @@ ng () {
 }
 
 res=0
-
+# 最初のテスト
 out=$(seq 5 | ./plus)
+++ seq 5
+++ ./plus
++ out=15
+[ "${out}" = "15" ] || ng "$LINENO"
++ '[' 15 = 15 ']'
 
-[ "${out}" = 15 ] || ng "$LINENO"
+# 無効な入力をテスト (ここで出力からWarningメッセージを無視)
+out=$(echo あ | ./plus)
+++ echo あ
+++ ./plus
++ out='Warning: '\''あ'\'' is not a valid number. Treated as 0.0
+0.0'
+# 期待される出力を「0.0」と比較
+[ "${out}" = "0.0" ] || ng "$LINENO"
++ '[' 0.0 = 0.0 ']'
 
-out=$(echo あ| ./plus)
-[ "$?" = 1]       || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
-
+# 空の入力をテスト
 out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+++ echo
+++ ./plus
++ out='0.0'
+[ "${out}" = "0.0" ] || ng "$LINENO"
++ '[' 0.0 = 0.0 ']'
 
+# 最後にテストの結果を確認
 [ "$res" = 0 ] && echo OK
++ '[' 0 = 0 ']'
+OK
 exit $res
++ exit 0
+
