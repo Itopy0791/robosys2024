330ff8aad9e57e591b2ada72cb7b35ddaff93a08
Yukifumi Itohiya <s23C1017KB@s.chibakoudai.jp>
2024-11-07 13:35:01 +0900

    Add or update test.bash

--- test.bash
+++ test.bash
@@ -1,35 +1,26 @@
 #!/bin/bash -xv
 
 ng () {
-        echo ${1}行目が違うよ
+        echo "${1}行目が違うよ"
 	res=1
 }
 
 res=0
+
 # 最初のテスト
 out=$(seq 5 | ./plus)
-+ out=15
 [ "${out}" = "15" ] || ng "$LINENO"
-+ '[' 15 = 15 ']'
 
 # 無効な入力をテスト (ここで出力からWarningメッセージを無視)
 out=$(echo あ | ./plus)
-+ out='Warning: '\''あ'\'' is not a valid number. Treated as 0.0
-0.0'
 # 期待される出力を「0.0」と比較
 [ "${out}" = "0.0" ] || ng "$LINENO"
-+ '[' 0.0 = 0.0 ']'
 
 # 空の入力をテスト
 out=$(echo | ./plus)
-+ out='0.0'
 [ "${out}" = "0.0" ] || ng "$LINENO"
-+ '[' 0.0 = 0.0 ']'
 
 # 最後にテストの結果を確認
 [ "$res" = 0 ] && echo OK
-+ '[' 0 = 0 ']'
-OK
 exit $res
-+ exit 0
 
